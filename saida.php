8cfa70a9a45be5e0c83b9e309b9feb9f7efe23b2
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-07-08 22:07:09 -0300

    php

---
+++ saida.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print " <h1>texto</h1> <br/>";
+        
+        print "<h2>Olá, mundo\n</h2> <br/>";
+        
+        print "Escape 'chars' são os MESMOS como em C\n <br/>" ;
+        print "Você pode ter quebras de linhas em uma string. <br/>";
+        print 'Uma string pode usar "aspas-duplas", isso é muito legal! <br/>';
+        print 'Ainda pode-se usar aspas simples dessa forma it\'s cool! <br/>';
+    ?>
+
+    <?php
+        echo " <h1>texto</h1> <br/>";
+        
+        echo "Olá, mundo\n <br/>";
+        echo "Escape 'chars' são os MESMOS como em C\n <br/>" ;
+        echo "Você pode ter quebras de linhas em uma string. <br/>";
+        echo 'Uma string pode usar "aspas-duplas", isso é muito legal! <br/>';
+        echo 'Ainda pode-se usar aspas simples dessa forma it\'s cool! <br/>';
+    ?>
+</body>
+</html>
