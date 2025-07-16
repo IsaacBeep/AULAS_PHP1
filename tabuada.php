9c7481e2e45fd7fe6369671be2b0e0caca6e73ac
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-07-15 22:07:43 -0300

    att

---
+++ tabuada.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num = 1;
+        $num2 = 1;
+        
+        while ($num <= 10){
+            $resu = $num * $num2;
+            echo "$resu <br>";
+            $num++;
+        }
+
+    ?>
+</body>
+</html>
