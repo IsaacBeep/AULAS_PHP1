9bf808f8698189b9e2f4d813a1eff9784f1ee714
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-07-17 22:08:54 -0300

    formularios

---
+++ Texto/AdicionarArquivo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $linhas = file ("texto.txt",FILE_IGNORE_NEW_LINES);
+        var_dump($linhas);
+        foreach ($linhas as $linhas_num => $linha){
+            echo "Linha #($linhas_num) ; ".$linha. " <br>";
+        }
+    ?>
+</body>
+</html>
