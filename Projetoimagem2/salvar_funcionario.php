f6fdf6b5410fd39b540a9fc110fef8aa0c47d545
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-08-07 21:16:39 -0300

    att

--- Projetoimagem2/salvar_funcionario.php
+++ Projetoimagem2/salvar_funcionario.php
@@ -1,83 +1,96 @@
 <?php
-function redimensionarImagem($imagem, $largura, $altura){
-    list($larguraOriginal, $alturaOriginal) = getimagesize($imagem);
+    function redimensionar ($imagem,$largura,$altura){
+        //Obtém as dimensões originais da imagem 
+        //Getimagesize() Retorna a largura e altura de uma imagem
+
+        list($largura,$alturaOriginal,$larguraOriginal) = getimagesize($imagem);
+
+        //Cria uma nova imagem de acordo com as novas dimensões
+        //imagecreatetruecolor = cria uma nova imagem em branco com alta qualidade
 
 
-//CRIA UMA NOVA IMAGEM EM BRANCO COM NOVAS DIMENSOES
-//imagecreatetruecolor() CRIA UMA NOVA IMAGEM EM BRANCO COM ALTA QUALIDADE
-    $novaImagem = imagecreatetruecolor($largura, $altura);
-    
-    //CARREGA A IMAGEM ORIGINAL(JPEG) APARTIR DO AQUIVO
-    //imagecreatefromjpeg() CRIA A IMAGEM PHP A PARTIR DE UM JPEG
-    $imagemOriginal = imagecreatefromjpeg($imagem);
-    
-    // COPIA E REDIMENSIONA A IMAGEM ORIGINAL PARA A NOVA 
-    //imagecopyresampled() COPIA E REDIMENSIONAMENTO E SUAVIZAÇÃO
-    imagecopyresampled($novaImagem, $imagemOriginal, 0, 0, 0, 0, $largura, $altura, $larguraOriginal, $alturaOriginal);
-    
-    //INICIA UM BUFFER PARA GUARDAR A IMAGEM COMO TEXTO BINARIO
-    // ob_start() INICIA O "output buffering" GUARDANDO A SAIDA
-    ob_start();
-    //imagejep()ENVIA A IMAGEM PARA O OUTPUT(QUEVAI PRO BUFFER)
-    imagejpeg($novaImagem);
+        $nova_imagem = imagecreatetruecolor($largura, $largura);
 
-    //OB_GET_CLEAN() -- PEGAR O CONTEUDO DO BUFFER E LIMPA O BUFFER
-    $dadosImagem = ob_get_clean();
+        //Carrega a imagem origina (jpeg) a partir do arquivp 
+        //imagecreatefromjpeg() cria a imagem php
 
-    //LIBERA A MEMÓRIA USADA PELAS IMAGEMS
-    //imagendestroy() LIBERA A MEMÓRIA USADA PELA IMAGEM
-    imagedestroy($novaImagem);
-    imagedestroy($imagemOriginal);
+        $imagemOriginal = imagecreatefromjpeg($imagem);
 
-    //RETORNA A IMAGEM REDIMENSIONADA EM FORMATO BINARIO
-    return $dadosImagem;
-}
-    //CONFIGURAÇÂO DO BANCO DE DADOS
-    $host = "localhost";
-    $dbname = "armazenaimagem";
-    $dbusername = "root";
-    $password = "";
+        //copia e redmensiona a imagem original para a nova 
+        //imagecopyresampled()-- copia o redimensionamento e suavização
 
-    try{
-        $pdo = new PDO("mysql:host=$host;dbname:$dbname", $username, $password);
-        $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERMODE_EXCEPTION);
+        imagecopyresampled($nova_imagem,$imagemOriginal,0,0,0,0,$largura,$altura,$larguraOriginal,$alturaOriginal);
 
-        if($_SERVER['REQUEST_METHOD']=='POST' && isset($_FILES('foto'))){
-            if($_FILES['foto']['error']==0){
+        //Inicia um buffer para guardar a imagem com texto binário
+        //ob-start()-- iniciar o "output buffering" guardando a saida
+
+        ob_start();
+
+        //imagejep()Envia a imagem para o output(que vai pro buffer)
+
+        imagejpeg($nova_imagem);
+
+        //Ob_get_clean() -- Pegar o conteudo do buffer e limpa
+        $dadosImagem =ob_clean();
+
+        //Libera a memória usada pelas imagens
+        //imagedestroy() -- limpa a memoria da imagem criada
+        imagedestroy($nova_imagem);
+        imagedestroy($imagemOriginal);
+
+        //Retorna a imagem redmensionada em formato binario
+        return $dadosImagem;
+
+    }
+
+    //Configuraçao do banco de dados
+    $host = 'localhost';
+    $dbname = 'armazena_imagem';
+    $username = 'root';
+    $password = '';
+
+    try {
+        //Conexao com o banco usando pdo
+        $pdo = new pdo("mysql:host=$host;dbname:$dbname",$username,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if($_SERVER['REQUEST_METHOD']=='POST'&& isset($_FILES['foto'])){
+
+            if($_FILES['foto']['error'] == 0){
                 $nome = $_POST['nome'];
                 $telefone = $_POST['telefone'];
-                $nomeFoto = $_FILES['foto']['name']; //pega o nome original do arquivo
-                $tipoFoto = $_FILES['foto']['type']; //pega o tipo mime da imagem
+                $imagem = $_FILES['foto']['name']; // PEGA O NOME ORIGINAL DO ARQUIVO
+                $tipo_imagem = $_POST['foto']['type'];  // PEGA O TIPO mime DA IMAGEM
 
-                //Redimensionar imgaem
-                // O codigo abaixo cuja variavel é "tmp_name" é o caminho temporario do arquivo 
-                $foto = redimensionarImagem($_FILES['foto']['tmp_name'],300.400);
+                // REDIMENSIONA A IMAGEM
+                // O CODIGO ABAIXO CUJA VARIAVEL É tpm_name É O CAMINHO TEMPORARIO DO ARQUIVO 
+                $imagem = redimensionar($_FILES['foto']['tmp_name'], 300,400);
 
-                //Insere no banco de dados usando sql preparad
-                $sql = "INSERT INTO funcionarios(nome,telefone,nome_foto,tipo_foto,foto) VALUES (:nome,:telefone,:nome_foto,:tipo_foto,:foto)";
-                $stmt = $pdo->prepare($sql); //Responsavel por preparar a query para evitar ataque de sql injection
-                $stmt->bindParam(':nome', $nome); //Liga os parametros as variaveis
-                $stmt->bindParam(':telefone', $telefone); //Liga os parametros as variaveis
-                $stmt->bindParam(':nome_foto', $nomeFoto); //Liga os parametros as variaveis
-                $stmt->bindParam(':tipo_foto', $tipoFoto); //Liga os parametros as variaveis
-                $stmt->bindParam(':foto', $foto , PDO::PARAM_LOB); //O lob - large Object Usado para dados binarios como imagem
-                if($stmt->execute()){
+                // INSERE NO BANCO E DADOS USANDO O SQL PREPARED
+                $sql = "INSERT INTO funcionarios (nome,telefone,nome_foto,tipo_foto,foto) VALUES(:nome, :telefone, :nome_foto, :tipo_foto, :foto)";
+                $stmt = $pdo->prepare($sql); // RESPONSAVEL POR PREPARAR A QUERY PARA EVITAR ATAQUE SQL INJECTION
+                $stmt->bindParam(':nome',$nome); // LIGA OS PARAMETROS ÀS VARIAVEIS
+                $stmt->bindParam(':telefone',$telefone); // LIGA OS PARAMETROS ÀS VARIAVEIS
+                $stmt->bindParam(':nome_foto',$imagem); // LIGA OS PARAMETROS ÀS VARIAVEIS
+                $stmt->bindParam(':tipo_foto',$tipo_imagem); // LIGA OS PARAMETROS ÀS VARIAVEIS
+                $stmt->bindParam(':foto', $imagem,PDO::PARAM_LOB); // LOB = LARGE OBJECT USADO PARA DADOS BINARIOS COMO IMAGEM 
+
+                if ($stmt->execute()){
                     echo "funcionario cadastrado com sucesso!";
-                } else{
-                    echo"Erro ao cadastrar o usuario";
+                } else {
+                    echo "Erro ao cadastrar o funcionario";
                 }
-
-            }else {
-                echo "Erro ao fazer upload da foto! Codigo: " . $_FILES['foto']['error'];
+            } else {
+                echo "Erro ao fazer upload da foto! Código: ". $_FILES['foto']['error'];
             }
         }
-    }catch(PDOException $e){
-        echo"Erro. " . $e->getMessage(); //Mostra o erro se houver
-    }
+       } catch(PDOException $e) {
+        echo "Erro. ".$e->getMessage(); // MOSTRA O ERRO SE HOUVER 
+       } 
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -86,7 +99,7 @@ function redimensionarImagem($imagem, $largura, $altura){
 <body>
     <h1>Lista de Imagens</h1>
 
-    <a href="consulta_fucionario.php">Listar funcionarios</a>
-    
+    <a href="consulta_funcionario.php">Listar Funcionarios</a>
+
 </body>
 </html>
